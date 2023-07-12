450f168563fc0557e9a627195167f230c88288f2
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-07-11 20:51:22 -0400

    lab complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        print(''.join(items))
+        return ''.join(items)
+    elif len(items) == 2:
+        return ' and '.join(items)
+    elif len(items) > 2:
+        print(', '.join(items[:-1]) + ', and ' + items[-1])
+        return ', '.join(items[:-1]) + ', and ' + items[-1]
+    
+oxford_comma(['kiwi'])
